4a4068c124983b09093d64b727199051ea265562
uma-victor1 <umavictor11@gmail.com>
2021-04-18 12:14:44 +0100

    Navigation component

---
+++ components/Navigation.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<header>
+			<nav class="bg-white px-8 pt-2 shadow-md">
+				<div class="-mb-px flex justify-center">
+					<NuxtLink
+						class="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8"
+						href="#"
+					>
+						Home
+					</NuxtLink>
+					<NuxtLink
+						class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8"
+						href="#"
+					>
+						Products
+					</NuxtLink>
+					<NuxtLink
+						class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8"
+						href="#"
+					>
+						Discounts
+					</NuxtLink>
+					<NuxtLink
+						class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3"
+						href="#"
+					>
+						Customers
+					</NuxtLink>
+				</div>
+			</nav>
+		</header>
+	</div>
+</template>
