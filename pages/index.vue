b7342d070c653ed4ef6123ac6999c83b1f643272
uma-victor1 <umavictor11@gmail.com>
2021-04-19 18:30:06 +0100

    style home page a bit

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
 	<div class="m-auto mt-8">
 		<div class="flex h-3/4">
-			<div class="w-3/4 pl-16">
-				<h1 class="text-4xl text-gray-800">Uma Victor</h1>
-				<br /><span class="text-gray-500">Software Engineer</span>
+			<div class="w-3/4 flex justiify-center items-center">
+				<div class="pl-16">
+					<h1 class="text-4xl text-gray-800">Uma Victor</h1>
+					<br /><span class="text-gray-500">Software Engineer</span>
+				</div>
 			</div>
-			<div class="h-full"><img src="../assets/my_img.jpg" alt="" /></div>
+			<div class="h-full w-1/2"><img src="../assets/my_img.jpg" alt="" /></div>
 		</div>
 	</div>
 </template>
