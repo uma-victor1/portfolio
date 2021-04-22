878e9cd05d504eb4947954dc7779f7f113323d2d
uma-victor1 <umavictor11@gmail.com>
2021-04-22 22:53:03 +0100

    start adding works

--- pages/works.vue
+++ pages/works.vue
@@ -1,5 +1,12 @@
 <template>
-	<div><h1>MY WORKS!!</h1></div>
+	<div class="p-14">
+		<h1 class="pb-6 text-2xl">MY WORKS!!</h1>
+		<p class="text-gray-500">
+			I have been working lately with vue alot. <br />Here are some of the
+			projects
+		</p>
+		<div><div></div></div>
+	</div>
 </template>
 
 <script>
